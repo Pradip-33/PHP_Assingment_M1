87c5c7b2e76b38398fce8587f9c256e43950a4ff
Pradip Suthar <pfsuthar33@gmail.com>
2024-08-14 19:09:23 +0530

    commit

---
+++ 1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Result</title>
+</head>
+<body>
+    <label>Physics</label>
+    <label>Chemistry</label>
+    <label>Biology</label>
+    <label>Mathematics</label>
+    <label>Computer</label>
+</body>
+</html>
