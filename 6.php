8f385a7d2047af9f7af0e4f4fc19aa2e7f8a7b6d
Pradip Suthar <pfsuthar33@gmail.com>
2024-08-17 09:22:59 +0530

    commit

--- 6.php
+++ 6.php
@@ -9,7 +9,31 @@
     <form action="#" method="post">
     <h2 align="Center"><b><u>Armstrong Number</u></b></h2>
     <label><b>Enter Number to find Armstrong</b></label><br>
-    <input type="text" name="arm" id="arm">
+    <input type="text" name="arm" id="arm"><br>
+    <input type="Submit" value="Submit" name="submit" >
     </form>
+    <?php
+
+$num = $_POST['arm'];
+$sum = 0;
+$temp = $num;
+
+
+$numDigits = floor(log10($num) + 1);
+
+while ($temp > 0) {
+    $digit = $temp % 10;
+    $sum += pow($digit, $numDigits);
+    $temp = floor($temp / 10);
+}
+
+if ($sum == $num) {
+    echo $num . "<br> is an Armstrong number.";
+} else {
+    echo $num . "<br> is not an Armstrong number.";
+}
+
+?>
+
 </body>
 </html>
