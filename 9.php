616c5c3ec6a23c4c3f0dfaf6534b0f6c80341b5a
Pradip Suthar <pfsuthar33@gmail.com>
2024-08-23 09:34:17 +0530

    Update 9.php

--- 9.php
+++ 9.php
@@ -19,30 +19,7 @@ $b = "2$b";
         
     ?>
     <h3>
-    
-
-
-
-1. $a = '1'; <br>
-   - The variable $a is assigned the string value'1'.<br>
-   - Value of $a: '1'<br>
-
-2. $b = &$a;<br> 
-   - The variable $b is assigned by reference to $a. This means $b is not just a copy of $a; instead, $b and $a now both point to the same memory location. Any change in $b will directly affect $a and vice versa.<br>
-   - Value of $b: '1' (because $b is referencing $a)<br>
-
-3. $b = "2$b";  <br>
-   - The variable $b is now being reassigned to a new value. The new value is a string that begins with '2' and is followed by the current value of $b.<br>
-   - Since $b is referencing $a, $b currently holds '1', so "2$b" becomes "21".<br>
-   - Therefore, $b is now assigned the string "21".<br>
-   - Because $b is a reference to $a, changing $b also changes $a to "21".<br>
-
-Final Values:<br>
-- Value of $a: "21"<br>
-- Value of $b: "21"<br>
-
- Summary:<br>
-Both $a and $b have the value "21" after the code is executed. This happens because $b was initially set as a reference to $a, so any change made to $b also updates $a.<br>
+:- $a and $b will be "21". This is because $b is a reference to $a, so when $b is updated to "2$b", $a is also updated to "21".
     </h3>
     
 </body>
